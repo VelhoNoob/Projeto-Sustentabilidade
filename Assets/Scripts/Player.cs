2680a205910204245684802145e15bdd2fb141ca
Velho Noob <65304365+VelhoNoob@users.noreply.github.com>
2025-02-18 20:24:02 -0300

    Movimento teste

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D rb;
+
+    private Vector2 moveDir;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var xInput = Input.GetAxisRaw("Horizontal");
+
+        //Configurando direções
+        if (rb.velocity.x > 0)
+            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+        if (rb.velocity.x < 0)
+            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
     }
+
 }
