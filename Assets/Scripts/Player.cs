f04379b661535cdab87297eddda7cf93d05c7152
Velho Noob <65304365+VelhoNoob@users.noreply.github.com>
2025-02-19 18:01:38 -0300

    Movimento e Assets

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,43 @@
-using System.Collections;
+ï»¿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private Vector2 movement;
 
-    private Vector2 moveDir;
-    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        var xInput = Input.GetAxisRaw("Horizontal");
+        // Captura a entrada do jogador
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
 
-        //Configurando direções
+    void FixedUpdate()
+    {
+        // Move o personagem
+        rb.velocity = movement.normalized * moveSpeed;
+
+        //Configurando direï¿½ï¿½es
         if (rb.velocity.x > 0)
-            transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+        {
+            //transform.localScale = new Vector3(1f, transform.localScale.y, transform.localScale.z);
+            sr.flipX = false;
+        }
         if (rb.velocity.x < 0)
-            transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
+        {
+            //transform.localScale = new Vector3(-1f, transform.localScale.y, transform.localScale.z);
+            sr.flipX = true;
+        }
     }
 
 }
