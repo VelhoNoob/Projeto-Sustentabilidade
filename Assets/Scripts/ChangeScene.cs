957fa2a184f929af727ca14544b3efc31a3e2934
Danilo Erler Lima <50208673+daniloelima@users.noreply.github.com>
2025-03-13 20:41:23 -0300

    mudanças mapa tutorial

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // Adicione esta linha
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // Método para mudar a cena
+    public void ChangeToScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    // Método para fechar o jogo
+    public void QuitGame()
+    {
+        Application.Quit();
+        // Para testar no editor do Unity
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
